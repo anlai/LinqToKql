24e2707a47ff4c6d3a8e944e604a21c983219388
Alan Lai <alan.n.lai@gmail.com>
2024-08-22 11:33:34 -0700

    test for chaining where clauses

--- LinqToKql.Test/Tests/ToKql_Combined.cs
+++ LinqToKql.Test/Tests/ToKql_Combined.cs
@@ -14,5 +14,15 @@ namespace LinqToKql.Test.Tests
 
             Assert.AreEqual("resources | where name == 'test' or name == 'test2' | sort name", kql);
         }
+
+        [TestMethod]
+        public void ToKql_WhereAndWhere_Success()
+        {
+            var q = Kql.Create<AzureResource>().Where(x => x.name == "test").Where(x => x.type == "microsoft.web/site");
+
+            var kql = q.ToKql();
+
+            Assert.AreEqual("resources | where name == 'test' | where type == 'microsoft.web/site'", kql);
+        }
     }
 }
