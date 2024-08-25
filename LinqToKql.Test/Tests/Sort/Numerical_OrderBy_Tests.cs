f323927010ef4680985383d7f9f75d8cd8e3f42f
Alan Lai <alan.n.lai@gmail.com>
2024-08-25 15:42:41 -0700

    numerical sorts

---
+++ LinqToKql.Test/Tests/Sort/Numerical_OrderBy_Tests.cs
@@ -0,0 +1,28 @@
+﻿using LinqToKql.Test.Models;
+
+namespace LinqToKql.Test.Tests.Sort
+{
+    [TestClass]
+    public class Numerical_OrderBy_Tests
+    {
+        [TestMethod]
+        public void ToKql_OrderByIntAsc_Success()
+        {
+            var q = Kql.Create<AzureResource>().OrderBy(x => x.instances);
+
+            var kql = q.ToKql();
+
+            Assert.AreEqual("resources | sort instances asc", kql);
+        }
+
+        [TestMethod]
+        public void ToKql_OrderByIntDesc_Success()
+        {
+            var q = Kql.Create<AzureResource>().OrderByDescending(x => x.instances);
+
+            var kql = q.ToKql();
+
+            Assert.AreEqual("resources | sort instances desc", kql);
+        }
+    }
+}
